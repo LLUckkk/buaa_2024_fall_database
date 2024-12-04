de40d2fcbd4a6ec5b8ab58b0a2a9033b326c7efb
Zhanghanwen <zhanghanwen@buaa.edu.cn>
2024-12-04 21:30:22 +0800

    modify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
+//import './plugins/element.js'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
